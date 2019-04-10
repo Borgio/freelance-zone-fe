b2634b8ff6ffedf9aa77580c55f8505e9e84de23
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-10 11:50:07 -0400

    initial commit

---
+++ src/app/homepage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  template: '<h1>Freelance Zone Dashboard</h1>'
+})
+export class HomepageComponent {
+  title = 'Freelance Zone Dashboard';
+ }
