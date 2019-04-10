285a88240b3acf55eda898b465211ed840760dae
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-10 15:43:51 -0400

    Mock document data form interface

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
-export class DocumentsComponent { }
+export class DocumentsComponent { 
+  documents: Document[] = [
+    {
+      title: 'first_doc',
+      description: 'first document',
+      file_url: 'http://google.com',
+      updated_at: '04/10/2019',
+      image_url: 'http://google.com',
+    },
+    {
+      title: 'second_doc',
+      description: 'second document',
+      file_url: 'http://google.com',
+      updated_at: '04/10/2019',
+      image_url: 'http://google.com',
+    },
+    {
+      title: 'third_doc',
+      description: 'third document',
+      file_url: 'http://google.com',
+      updated_at: '04/10/2019',
+      image_url: 'http://google.com',
+    }
+  ]
+}
