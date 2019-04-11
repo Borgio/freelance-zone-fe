7a45f0177b5125bc41c5241aefbc05ecc87f54a7
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-10 22:31:16 -0400

    Proposal mock data

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent { }
+export class ProposalListComponent { 
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'https://rubypythmore.com',
+    'Ruby on Rails', 150, 120, 15, 'scott.goyette@rubypythmore.com')
+  proposalTwo: Proposal = new Proposal(99, 'Abc Company', 'https://rubypythmore.com',
+    'Ruby on Rails', 150, 120, 15, 'scott.goyette@rubypythmore.com')
+  proposalThree: Proposal = new Proposal(300, 'Abc Company', 'https://rubypythmore.com',
+    'Ruby on Rails', 150, 120, 15, 'scott.goyette@rubypythmore.com')
+
+  proposals: Proposal[] = [
+    this.proposalOne, 
+    this.proposalTwo, 
+    this.proposalThree
+  ]
+}
